8088c3137caa29264d5b14f0f45cc0cdddd6ead2
Mayurmk <mayurkapadia35@gmail.com>
2019-01-22 11:22:32 +0530

    Update server.js
    Commiit for the hii

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
 // const moment = require('moment');
-
+const hello = 'hiii';
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
